75ea0a3e6390e3bf037a41f77aac756af268556d
Jamancode <71838108+Jamancode@users.noreply.github.com>
2020-09-27 21:54:20 +0200

    commit on master

---
+++ Python_lernen/005_While-Schleifen.py
@@ -0,0 +1,13 @@
+#als Beispiel zuerst eine if anweisung um den Direkten unterschied zusehen.
+
+spam = 0
+if spam < 5:
+    print('Jaman!')
+    spam = spam + 1
+
+
+
+spam = 0 
+while spam < 5:
+    print('Wooohhhooo')
+    spam = spam + 1
